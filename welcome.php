34dc5023cf5c1dd0376f910b3583047164fa88ec
tuti <trupti.diwani@qed42.com>
2019-01-20 22:26:38 +0530

    Php Forms Completed

---
+++ welcome.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<!-- Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?> -->
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
